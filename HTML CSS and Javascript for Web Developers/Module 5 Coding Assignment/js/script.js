8ef4c82f26b544cd54af197f13d7ed032ccaf0dc
Hasanul Bari <39557895+Hasanul-Bari@users.noreply.github.com>
2020-07-17 22:10:49 +0600

    test 4

--- HTML CSS and Javascript for Web Developers/Module 5 Coding Assignment/js/script.js
+++ HTML CSS and Javascript for Web Developers/Module 5 Coding Assignment/js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       console.log(chosenCategoryShortName);
 

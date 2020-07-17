8bd1e840dc760ae031c1607a9c3edc7bab577477
Hasanul Bari <39557895+Hasanul-Bari@users.noreply.github.com>
2020-07-17 23:43:28 +0600

    test 7

--- HTML CSS and Javascript for Web Developers/Module 5 Coding Assignment/js/script.js
+++ HTML CSS and Javascript for Web Developers/Module 5 Coding Assignment/js/script.js
@@ -130,7 +130,7 @@ function buildAndShowHomeHTML (categories) {
 
       console.log(chosenCategoryShortName);
 
-      var homeHtmlToInsertIntoMainPage =insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage =insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
